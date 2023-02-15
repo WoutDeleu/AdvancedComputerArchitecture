644c83f56e02d071eb96653ca3fa340ee2bbab8d
blurfquarf <vanlauwemaxou@gmail.com>
2023-02-15 15:40:04 +0100

    Finish lab1

--- Lab1.py
+++ Lab1.py
@@ -3,7 +3,7 @@ import time
 import numpy as np
 from numba import cuda
 
-N = 4000
+N = 1000000
 input_arr = np.arange(N)
 output_arr = np.zeros_like(input_arr)
 
@@ -14,25 +14,25 @@ output_cpu = output_arr
 # Define kernel (GPU) function ...
 @cuda.jit
 def flip(input_arr, output_arr):
-    i = cuda.threadIdx.x + 1024 * (cuda.blockIdx.x -1)
-    temp = input_arr[i]
+    i = cuda.threadIdx.x + 1024 * (cuda.blockIdx.x - 1)
     output_arr[i] = input_arr[N - i - 1]
-    output_arr[N - 1 - i] = temp
+
 
 # Call kernel function ...
 # Time it
-numberBlocks = math.ceil((N/2) / 1024)
-flip[numberBlocks, int(N/2)](input_arr, output_arr)
+numberBlocks = math.ceil(N / 1024)
+numberThreads = N / numberBlocks;
+flip[numberBlocks, numberBlocks](input_arr, output_arr)
 start = time.time()
-#1 block, N/2 threads
-#code uitgevoerd voor N/2 threads
-flip[numberBlocks, int(N/2)](input_arr, output_arr)
+# 1 block, N threads
+# code uitgevoerd voor N/2 threads
+flip[numberBlocks, numberBlocks](input_arr, output_arr)
 cuda.synchronize()
 total = time.time() - start
 print("GPU RESULTS:")
 print(total)
-#print(input_arr)
-#print(output_arr)
+# print(input_arr)
+# print(output_arr)
 print()
 
 start = time.time()
@@ -43,5 +43,5 @@ for i in range(0, int(N / 2)):
 total = time.time() - start
 print("CPU RESULTS:")
 print(total)
-#print(input_cpu)
-#print(output_cpu)
+# print(input_cpu)
+# print(output_cpu)
