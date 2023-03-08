ba4b9b8f725a18a249332c7196fd60423c7c7a00
Steven Impens <s.impens.dev@gmail.com>
2023-03-08 16:07:47 +0100

    sum_columns_matrix_atomic

--- Lab2-3.py
+++ Lab2-3.py
@@ -1,9 +1,13 @@
+import math
+
 import numpy as np
 from numba import cuda
 
-sizeArray = 4
-numberBlocks = 1
-numberThreads = [sizeArray, sizeArray]
+sizeArray = 10000
+number_blocks_x = math.ceil(sizeArray / 1024)
+number_blocks_y = math.ceil(sizeArray / 1024)
+block_size = [32, 32]
+amount_of_blocks = [number_blocks_x, number_blocks_y]
 
 
 @cuda.jit
@@ -13,8 +17,9 @@ def sum_columns_matrix_atomic(matrix, summed):
 
 
 summed_array = np.zeros(sizeArray)
-matrix = np.random.randint(10, size=(sizeArray, sizeArray))
-sum_columns_matrix_atomic[numberBlocks, numberThreads](matrix, summed_array)
+input_matrix = np.random.randint(10, size=(sizeArray, sizeArray))
+sum_columns_matrix_atomic[amount_of_blocks, block_size](input_matrix, summed_array)
 
-print(matrix)
+print(input_matrix)
+print()
 print(summed_array)
