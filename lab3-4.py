5f97f4599cafd1f5804a3021d84b60e34b00431c
Wout Deleu <80529253+WoutDeleu@users.noreply.github.com>
2023-03-29 17:50:45 +0200

    Plot + timing

--- lab3-4.py
+++ lab3-4.py
@@ -2,6 +2,7 @@ import math
 import sys
 from timeit import timeit
 import pandas as pd
+import matplotlib.pylab as plt
 
 
 import numpy as np
@@ -61,12 +62,12 @@ def matrix_multiplication_GPU_shared_memory(A, B, resulting_matrix):
 
 size = 1
 
-timing_cpu = pd.Series(dtype=float)
-timing_gpu_naive = pd.Series(dtype=float)
-timing_gpu_shm = pd.Series(dtype=float)
+timing_cpu = {}
+timing_gpu_naive = {} 
+timing_gpu_shared = {}
 
 while size < 100000:
-    print("size: {}".format(size))
+    print("size: ", format(size))
     block_size = 32, 32
     # 1024 threads per block
     number_blocks_x = math.ceil(size / block_size[0])
@@ -81,26 +82,49 @@ while size < 100000:
 
     # print(A)
     # print(B)
-
-    cpu = lambda: matrix_multiplication_CPU(A, B, resulting_matrix)
-    pd.concat([timing_cpu, pd.Series(timeit(cpu, number=1000))])
+    # todo: aanvullen (groter dan 1000)?
+    if(size < 100): 
+        cpu = lambda: matrix_multiplication_CPU(A, B, resulting_matrix)
+        timing_cpu[size] = timeit(cpu, number=1000)
     # print("CPU")
     # print(resulting_matrix)
 
     gpu_naive = lambda: matrix_multiplication_GPU[amount_of_blocks, block_size](A, B, resulting_matrix)
-    pd.concat([timing_gpu_naive, pd.Series(timeit(gpu_naive, number=1000))])   # print("GPU")
+    timing_gpu_naive[size] = timeit(gpu_naive, number=1000)
+    
+    # print("GPU")
     # print(resulting_matrix)
-    # sys.stdout.flush()
 
-    gpu_shm = lambda: matrix_multiplication_GPU_shared_memory[amount_of_blocks, block_size](A, B, resulting_matrix)
-    pd.concat([timing_gpu_shm, pd.Series(timeit(gpu_shm, number=1000))])
+    gpu_shared = lambda: matrix_multiplication_GPU_shared_memory[amount_of_blocks, block_size](A, B, resulting_matrix)
+    timing_gpu_shared[size] = timeit(gpu_shared, number=1000)
     # print("Shared Memory")
     # print(resulting_matrix)
-    # sys.stdout.flush()
     size *= 2
-    if (size > 100):
-        break
 
 print(timing_cpu)
 print(timing_gpu_shm)
 print(timing_gpu_naive)
+
+# x-as: size van de dictionaries
+sizes = list(timing_gpu_shm.keys())
+
+# y-as: tijden van de drie dictionaries
+cpu_times = list(timing_cpu.values())
+gpu_naive_times = list(timing_gpu_naive.values())
+gpu_shared_times = list(timing_gpu_shared.values())
+
+# Plot de drie lijnen op dezelfde grafiek
+plt.plot(sizes, cpu_times, label='CPU')
+plt.plot(sizes, gpu_naive_times, label='GPU Naive')
+plt.plot(sizes, gpu_shared_times, label='GPU Shared Memory')
+
+# Voeg de legenda toe
+plt.legend()
+
+# Voeg titel en labels toe
+plt.title('Times for different implementations')
+plt.xlabel('Size')
+plt.ylabel('Time (ms)')
+
+# Toon de grafiek
+plt.show()
